00814c9ca014b43a5cb2974887981aed4a62724f
Grantjos12 <grantjoslyn@gmail.com>
2021-02-08 16:00:04 -0600

    changed port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const passport = require("passport");
